c89f27387a475897b22c67cb7befd0549baa89c6
OneNut <15757311067@163.com>
2022-05-03 10:42:16 +0800

    项目初始化，调整初始目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,80 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <h1>Etake快递代取平台</h1>
+    <nav>
+      <van-tabbar v-model="active" @change="onChange">
+        <van-tabbar-item>
+          <span>包裹</span>
+          <template #icon="props">
+            <img
+              :src="
+                props.active
+                  ? require('./assets/pkgctivate.png')
+                  : require('./assets/pkginactivate.png')
+              "
+            />
+          </template>
+        </van-tabbar-item>
+        <van-tabbar-item>
+          <span>广场</span>
+          <template #icon="props">
+            <img
+              :src="
+                props.active
+                  ? require('./assets/squareactivate.png')
+                  : require('./assets/squareinactivate.png')
+              "
+            />
+          </template>
+        </van-tabbar-item>
+        <van-tabbar-item>
+          <span>订单</span>
+          <template #icon="props">
+            <img
+              :src="
+                props.active
+                  ? require('./assets/orderactivate.png')
+                  : require('./assets/orderinactivate.png')
+              "
+            />
+          </template>
+        </van-tabbar-item>
+        <van-tabbar-item>
+          <span>我的</span>
+          <template #icon="props">
+            <img
+              :src="
+                props.active
+                  ? require('./assets/meactivate.png')
+                  : require('./assets/meinactivate.png')
+              "
+            />
+          </template>
+        </van-tabbar-item>
+      </van-tabbar>
+    </nav>
+  </div>
+
+  <router-view />
 </template>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+import { ref } from "vue";
+import { Toast } from "vant";
+export default {
+  name:'App',
+  setup() {
+    const active = ref(0);
+    const icon = {
+      active: "https://img.yzcdn.cn/vant/user-active.png",
+      inactive: "https://img.yzcdn.cn/vant/user-inactive.png",
+    };
+    const onChange = (index) => Toast(`标签 ${index}`);
+    return {
+      icon,
+      active,
+      onChange,
+    };
+  },
+};
+</script>
+<style lang = "less"></style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Tabbar, TabbarItem } from 'vant';
+import './styles/index.less'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Tabbar).use(TabbarItem).mount('#app')
